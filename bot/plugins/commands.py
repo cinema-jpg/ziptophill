9d5a652d44520906160d530159887b88bcc44aec
cinema-jpg <85360750+cinema-jpg@users.noreply.github.com>
2021-08-19 17:38:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕ Our Channel ⭕', url="https://t.me/cinephilediary_movies"
                                 )
                         ]
                     ]
